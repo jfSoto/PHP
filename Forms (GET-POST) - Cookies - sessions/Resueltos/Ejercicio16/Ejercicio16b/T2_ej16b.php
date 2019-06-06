1a5910e270fd1211edd6b2841813a29cc434ce84
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:02:29 +0200

    Create T2_ej16b.php

---
+++ Forms (GET-POST) - Cookies - sessions/Resueltos/Ejercicio16/Ejercicio16b/T2_ej16b.php
@@ -0,0 +1,15 @@
+<!-- 
+    Crea 3 script. En el primero, se crea una variable de sesión, se almacena, se
+    imprime su valor y se muestra un enlace al siguiente. En el segundo, se accede
+    a la variable de sesión, se destruye y se muestra un enlace al tercero. En el
+    tercero, se intenta acceder a la variable. 
+-->
+
+<?php
+    session_start();
+    $var = $_SESSION['var'];
+    print("Valor de la variable de sesión: $var");
+    unset($_SESSION['var']);
+?>
+
+<br><a href="T2_ej17c.php">Siguiente</a>
