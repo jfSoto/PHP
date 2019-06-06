451209014d4764b3d58aac05a6fea45da4429fd5
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:17:25 +0200

    Create Ejercicio02.php

---
+++ Basic PHP/Resueltos/Ejercicio02/Ejercicio02.php
@@ -0,0 +1,12 @@
+<!-- 
+    Crea una página PHP con cuatro variables numéricas y calcule el valor medio de las 4.
+    Imprime el resultado por pantalla.
+-->
+
+<?php
+    $var1 = 17;
+    $var2 = 1;
+    $var3 = 2018;
+    $var4 = 0;
+    $valor_medio = ($var1 + $var2 + $var3 + $var4)/4;
+?>
