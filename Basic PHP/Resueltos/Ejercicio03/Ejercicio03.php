bf95a1fa332b903f3790c49a68573452ed10728c
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:19:11 +0200

    Create Ejercicio03.php

---
+++ Basic PHP/Resueltos/Ejercicio03/Ejercicio03.php
@@ -0,0 +1,41 @@
+<!-- 
+    Indique si los siguientes identificadores de variables en php son o no correctos, y en el
+    caso de que no lo sean, por qué.
+
+    ➔ $miNombre : Correcto.
+
+    ➔ $mi nombre : Incorrecto, porque " " no es un carácter válido.
+
+    ➔ miNombre : Incorrecto, porque las variables empiezan por '$'.
+
+    ➔ $nombre_de_4_usuarios : Correcto.
+
+    ➔ $4usuarios : Incorrecto, porque tras '$' no puede ir un número.
+
+    ➔ $los.nombre.de.todos : Incorrecto, porque "." no es un carácter
+    válido.
+
+    ➔ $nombreDeMiNiña : Correcto.
+
+    ➔ $echo : Correcto.
+
+    ➔ $variable_1 : Correcto.
+
+    ➔ $4variable : Incorrecto, porque tras '$' no puede ir un número.
+
+    ➔ $_4variable : Correcto.
+
+    ➔ $variable4 : Correcto.
+
+    ➔ %otra : Incorrecto, porque las variables empiezan por '$'.
+
+    ➔ $1_otra : Incorrecto, porque tras '$' no puede ir un número.
+
+    ➔ $variable_de_nombre_muy_largo : Correcto.
+
+    ➔ $ABC : Correcto.
+
+    ➔ $ A B C : Incorrecto, porque " " no es un carácter válido.
+
+    ➔ $A_y_B_x_C : Correcto. 
+-->
