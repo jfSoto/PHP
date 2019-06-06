51d271e27dda74af5b8ea0b9020a2da135e9971f
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:21:43 +0200

    Create Ejercicio06.php

---
+++ Basic PHP/Resueltos/Ejercicio06/Ejercicio06.php
@@ -0,0 +1,18 @@
+<!-- Crea un array con los nombres de las provincias de Andalucía de tres maneras diferentes. -->
+
+<?php
+    $provincias1 = array("Sevilla", "Córdoba", "Huelva", "Cádiz", "Jaén",
+    "Málaga", "Granada", "Almería");
+    
+    $provincias2 = ["Sevilla", "Córdoba", "Huelva", "Cádiz", "Jaén", "Málaga",
+    "Granada", "Almería"]
+
+    $provincias3[0] = "Sevilla";
+    $provincias3[1] = "Córdoba";
+    $provincias3[2] = "Huelva";
+    $provincias3[3] = "Cádiz";
+    $provincias3[4] = "Jaén";
+    $provincias3[5] = "Málaga";
+    $provincias3[6] = "Granada";
+    $provincias3[7] = "Almería");
+?>
