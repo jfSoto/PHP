257fb10a2208fbc57996379032c26dad9c7a8428
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 08:56:07 +0200

    Create Ejercicio1.php

---
+++ Basic PHP/Ejercicios/Ejercicio1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+    <head>
+    <?php
+        $cadena1 = "Hola, ";
+        $cadena2 = "qué tal.";
+        echo $cadena1.$cadena2;
+
+    ?>
+
+    </body>
+</html>
