1d33755c7aad930ab6f51f7cd59a16a11c6e7745
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:16:42 +0200

    Create Ejercicio01.php

---
+++ Basic PHP/Resueltos/Ejercicio01/Ejercicio01.php
@@ -0,0 +1,7 @@
+<!-- Concatena dos cadenas con el operador punto (.) e imprime su resultado. -->
+
+<?php
+    $cadena1 = "Cadena 1";
+    $cadena2 = "Cadena 2";
+    $cadena_concatenada = $cadena1 . $cadena2;
+?>
