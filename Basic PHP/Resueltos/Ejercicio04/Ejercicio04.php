4b720bc7700dcc77e5050054424e49f3f0582e67
José Francisco Soto <35599612+jfSoto@users.noreply.github.com>
2019-06-06 12:19:56 +0200

    Create Ejercicio04.php

---
+++ Basic PHP/Resueltos/Ejercicio04/Ejercicio04.php
@@ -0,0 +1,11 @@
+<!-- 
+    Guarda un número en dos variables diferentes, una de tipo cadena y otra de tipo numérico.
+    Luego comprueba si son iguales y si son idénticas. 
+-->
+
+<?php
+    $var_numero = 2;
+    $var_cadena = "2";
+    $son_iguales = $var_numero == $var_cadena;
+    $son_identicos = $var_numero === $var_cadena;
+?>
